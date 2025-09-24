95b68d306c8fcfdd4811d9ba458370ead8a69c02
ra-perzyna <adam.perzyna@amperart.pl>
2025-09-24 16:46:18 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { createClient } from '@/lib/supabase-browser';
+import { createClient } from '../../lib/supabase-browser';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
